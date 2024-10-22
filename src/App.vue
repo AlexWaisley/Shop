1de24a407c66ef6bfa1029ee8cc84df5adc8fdfe
Akyu <alexwaisley@gmail.com>
2024-10-22 12:41:29 +0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import PageContainer from '@main/PageContainer.vue';
+import Top from '@top/Top.vue';
+import Bottom from '@bottom/Bottom.vue';
+
+</script>
+
+<template>
+    <Top></Top>
+    <PageContainer></PageContainer>
+    <Bottom></Bottom>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import * as path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@storage': path.resolve(__dirname, './src/storage'),
+      '@models': path.resolve(__dirname, './src/models'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@main': path.resolve(__dirname, './src/components/main'),
+      '@top': path.resolve(__dirname, './src/components/top'),
+      '@bottom': path.resolve(__dirname, './src/components/bottom'),
+    }
+  }
+})
